74fa1cc41208f4f2c3ce0e9b58a9e446df85fdd1
windiest <kalone.cool@163.com>
2019-01-21 11:10:28 +0800

    蓝梦的提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      GGGGGGGGGG
+      H W
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>ABC</h3>
